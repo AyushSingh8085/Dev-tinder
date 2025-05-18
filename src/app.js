db8abc20d770bc082ad33e84f8562864a945a4ab
Ayush Singh <Ayush.Singh@wallero.com>
2025-05-18 22:29:32 +0530

    order of the code matters a lot

--- src/app.js
+++ src/app.js
@@ -2,7 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 7777;
 
+// Over-rides the any paths after this with own  (Keep the order of the code)
+
+// app.use("/", (req, res) => {
+//   res.send("Hello World");
+// });
+
 app.use("/test", (req, res) => {
+  res.send("Hello World");
+});
+
+// Correct position
+app.use("/", (req, res) => {
   res.send("Hello World");
 });
 
