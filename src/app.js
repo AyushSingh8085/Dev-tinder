1c0054a33a80bca0b99094c68b2423b360ea4c18
Ayush Singh <Ayush.Singh@wallero.com>
2025-06-03 22:28:46 +0530

    feat: add socket.io integration for real-time messaging and chat functionality

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const http = require("http");
+
+require("./utils/cronJob");
 
 const PORT = process.env.PORT || 3000;
 
@@ -20,16 +23,21 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
+const initializeSocket = require("./utils/socket");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
 
+const server = http.createServer(app);
+
+initializeSocket(server);
+
 connectDB()
   .then(() => {
     console.log("Database connected successfully");
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`Server is running on PORT ${PORT}`);
     });
   })
