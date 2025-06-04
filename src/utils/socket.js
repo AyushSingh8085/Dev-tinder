4dca02648a63c524967d411562c2d3c16123d1e1
Ayush Singh <Ayush.Singh@wallero.com>
2025-06-04 13:17:19 +0530

    feat: implement chat functionality with message handling and connection validation

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,7 @@
 const socket = require("socket.io");
 const crypto = require("crypto");
+const { Chat } = require("../models/chat");
+const ConnectionRequestModel = require("../models/connectionRequest");
 
 const getSecretRoomId = (userId, targetUserId) => {
   return crypto
@@ -21,10 +23,62 @@ const initializeSocket = (server) => {
       socket.join(roomId);
     });
 
-    socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
-      const roomId = getSecretRoomId(userId, targetUserId);
-      io.to(roomId).emit("messageReceived", { firstName, text, _id: userId });
-    });
+    socket.on(
+      "sendMessage",
+      async ({ firstName, userId, targetUserId, text }) => {
+        try {
+          const roomId = getSecretRoomId(userId, targetUserId);
+
+          const result = ConnectionRequestModel.findOne({
+            $or: [
+              {
+                fromUserId: userId,
+                toUserId: targetUserId,
+                status: "accepted",
+              },
+              {
+                fromUserId: targetUserId,
+                toUserId: userId,
+                status: "accepted",
+              },
+            ],
+          });
+
+          if (!result) {
+            return res.status(400).json({
+              message: "User is not your connection",
+            });
+          }
+
+          let chat = await Chat.findOne({
+            participants: { $all: [userId, targetUserId] },
+          });
+
+          if (!chat) {
+            chat = new Chat({
+              participants: [userId, targetUserId],
+              messages: [],
+            });
+          }
+
+          chat.messages.push({
+            senderId: userId,
+            text,
+          });
+
+          await chat.save();
+
+          io.to(roomId).emit("messageReceived", {
+            firstName,
+            text,
+            _id: userId,
+            timeStamp: new Date(),
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    );
 
     socket.on("disconnect", () => {});
   });
